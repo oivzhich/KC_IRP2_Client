42ff70afca8c9af4be6df20a3aca0b860441cd34
Volha Iuzhychenka <Volha_Iuzhychenka@epam.com>
2021-12-25 11:58:28 +0100

    Client v1

--- KC_IRP2_Client/KC_IRP2_Client.cpp
+++ KC_IRP2_Client/KC_IRP2_Client.cpp
@@ -8,15 +8,16 @@
 
 using namespace std;
 
-int main() {
+void main()
+{
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	int err;
 	wVersionRequested = MAKEWORD(2, 2);
 	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0) { return 0; }
-	
-	while (true) {
+	if (err != 0) { return; }
+	while (true)
+	{
 		SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 		// указание адреса и порта сервера
 		sockaddr_in dest_addr;
@@ -28,11 +29,11 @@ int main() {
 		cout << "Enter the string:" << endl;
 		fgets(buf, sizeof(buf), stdin);
 		send(s, buf, 100, 0);
-		if (recv(s, buf, sizeof(buf), 0) != 0) {
+		if (recv(s, buf, sizeof(buf), 0) != 0)
+		{
 			cout << "Poluchenaya stroka:" << endl << buf << endl;
 		}
 		closesocket(s);
 	}
 	WSACleanup();
-	return 0;
 }
