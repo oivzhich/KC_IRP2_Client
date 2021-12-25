e0d7b3fb5b396346a4f8278a7e70481a4a591883
Volha Iuzhychenka <Volha_Iuzhychenka@epam.com>
2021-12-25 12:24:46 +0100

    Client: choose product menu

--- KC_IRP2_Client/KC_IRP2_Client.cpp
+++ KC_IRP2_Client/KC_IRP2_Client.cpp
@@ -8,16 +8,18 @@
 
 using namespace std;
 
-void main()
-{
+void main() {
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	int err;
 	wVersionRequested = MAKEWORD(2, 2);
 	err = WSAStartup(wVersionRequested, &wsaData);
 	if (err != 0) { return; }
-	while (true)
-	{
+	while (true) {
+		char buf[256],
+			main_menu_choise,
+			switch_menu_choise;
+
 		SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 		// указание адреса и порта сервера
 		sockaddr_in dest_addr;
@@ -25,15 +27,32 @@ void main()
 		dest_addr.sin_port = htons(1280);
 		dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 		connect(s, (sockaddr*)&dest_addr, sizeof(dest_addr));
-		char buf[100];
-		cout << "Enter the string:" << endl;
+
+		//Выбор пункта меню и отправка его серверу
+		puts("\nChoose:\n");
+		puts("1 - Individual task");
+		puts("2 - Redact");
+		puts("3 - View");
+		puts("4 - Exit\n");
+		printf("Input: ");
+
 		fgets(buf, sizeof(buf), stdin);
-		send(s, buf, 100, 0);
-		if (recv(s, buf, sizeof(buf), 0) != 0)
-		{
-			cout << "Poluchenaya stroka:" << endl << buf << endl;
+		buf[1] = '\0';
+		send(s, buf, sizeof(buf), 0);
+		main_menu_choise = buf[0];
+
+		switch (main_menu_choise) {
+		case '1'://Выбрать
+			printf("\nInput necessary product name: ");
+			fgets(buf, sizeof(buf), stdin);
+			send(s, buf, sizeof(buf), 0);
+			recv(s, buf, sizeof(buf), 0);
+			printf("\nAuthor: %s\n", buf);
+			break;
+		default:
+			closesocket(s);
+			exit(0);
 		}
-		closesocket(s);
 	}
 	WSACleanup();
 }
