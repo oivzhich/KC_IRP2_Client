e18ef6d84e62200e645e44b94f76e7d7fa41748a
Volha Iuzhychenka <Volha_Iuzhychenka@epam.com>
2021-12-26 22:11:54 +0100

    Search and view all added

--- KC_IRP2_Client/KC_IRP2_Client.cpp
+++ KC_IRP2_Client/KC_IRP2_Client.cpp
@@ -4,6 +4,7 @@
 #include<WinSock2.h>
 #include<iostream>
 #include<stdlib.h>
+#include <string>
 #pragma warning(disable: 4996)
 
 using namespace std;
@@ -15,42 +16,58 @@ void main() {
 	wVersionRequested = MAKEWORD(2, 2);
 	err = WSAStartup(wVersionRequested, &wsaData);
 	if (err != 0) { return; }
+
+	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// указание адреса и порта сервера
+	sockaddr_in dest_addr;
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = htons(1280);
+	dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	connect(server_socket, (sockaddr*)&dest_addr, sizeof(dest_addr));
+
 	while (true) {
-		char buf[256],
+		char buf[500],
 			main_menu_choise,
 			switch_menu_choise;
-
-		SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
-		// указание адреса и порта сервера
-		sockaddr_in dest_addr;
-		dest_addr.sin_family = AF_INET;
-		dest_addr.sin_port = htons(1280);
-		dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-		connect(s, (sockaddr*)&dest_addr, sizeof(dest_addr));
+		string result;
 
 		//Выбор пункта меню и отправка его серверу
 		puts("\nChoose:\n");
-		puts("1 - Individual task");
-		puts("2 - Redact");
-		puts("3 - View");
-		puts("4 - Exit\n");
+		puts("1 - Search books by author");
+		puts("2 - View all");
+		puts("3 - Edit");
+		puts("4 - Add");
+		puts("5 - Exit\n");
 		printf("Input: ");
 
+		memset(&buf[0], 0, sizeof(buf));
 		fgets(buf, sizeof(buf), stdin);
 		buf[1] = '\0';
-		send(s, buf, sizeof(buf), 0);
+		send(server_socket, buf, sizeof(buf), 0);
 		main_menu_choise = buf[0];
 
 		switch (main_menu_choise) {
-		case '1'://Выбрать
-			printf("\nInput necessary product name: ");
+		case '1'://поиск книги по автору
+			memset(&buf[0], 0, sizeof(buf));
+			printf("\nEnter author name: ");
 			fgets(buf, sizeof(buf), stdin);
-			send(s, buf, sizeof(buf), 0);
-			recv(s, buf, sizeof(buf), 0);
-			printf("\nAuthor: %s\n", buf);
+			send(server_socket, buf, sizeof(buf), 0);
+			memset(&buf[0], 0, sizeof(buf));
+			recv(server_socket, buf, sizeof(buf), 0);
+			result = buf;
+			result.pop_back();
+			cout << "Books of author: " << endl << result << endl;
+			break;
+		case '2'://просмотреть все книги
+			memset(&buf[0], 0, sizeof(buf));
+			//получаем список всех книг
+			recv(server_socket, buf, sizeof(buf), 0);
+			result = buf;
+			result.pop_back();
+			cout << "All books:" << endl << result << endl;
 			break;
 		default:
-			closesocket(s);
+			closesocket(server_socket);
 			exit(0);
 		}
 	}
